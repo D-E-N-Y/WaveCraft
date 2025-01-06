9e7ed879ab1981e25c7af2133802139f85ea8457
D-E-N-Y <mysliaev.daniil@gmail.com>
2025-01-06 15:35:02 +0200

    upd: create processors script

--- Assets/Scripts/Actor/Building/Industrial/Processors/I_Processors.cs
+++ Assets/Scripts/Actor/Building/Industrial/Processors/I_Processors.cs
@@ -1,7 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class I_Processors : B_Industrial
 {
     [SerializeField] float factor;
     [SerializeField] float timeProcess;
+
+    private bool isProcessing;
+    private int amount;
+
+    private void StartProcess()
+    {
+        StartCoroutine(nameof(Processing));
+        isProcessing = true;
+    }
+
+    private IEnumerator Processing()
+    {
+        yield return new WaitForSeconds(timeProcess);
+
+        CompleteProcess();
+    }
+
+    private void CompleteProcess()
+    {
+        amount--;
+        
+        if(amount > 0) 
+            StartProcess();
+        else
+            isProcessing = false;
+
+        // ResourceSystem.AddResource();
+        
+    }
+
+    public void AddResource(int amount)
+    {
+        this.amount += amount;
+
+        if(!isProcessing) StartProcess();
+    }
 }
