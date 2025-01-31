70f683cc59a223d449e98ca8bdb5f4c5a4f229d7
D-E-N-Y <mysliaev.daniil@gmail.com>
2025-01-31 15:39:38 +0200

    upd: add resource prefabs

---
+++ Assets/Scripts/Actor/Resource/Resource.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Resource : Actor
+{
+    [SerializeField] private E_Resource resource;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+
+        currentHP = Mathf.Max(currentHP - damage, 0);
+
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
