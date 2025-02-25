15d37b0b2d17bdfe0482d446524d5eb34b9cc406
D-E-N-Y <mysliaev.daniil@gmail.com>
2025-02-25 12:14:54 +0200

    upd: UI maket interactable worker

---
+++ Assets/UI_TaskWorker.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TaskWorker : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI taskOrderText;
+    // [SerializeField] private 
+}
